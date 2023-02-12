cb032d0303336453dedbc4962ac4c87875f771e8
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-12 21:51:45 +0530

    I Updated  the player movememt

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 20f;
+    // Variable Declaration
+    float horizontalInput;  //for the horizontal input
+    float xRange = 20f;    //for the range to bound the player to the screen so that it should not fall down 
+    float speed = 20f;    // speed for the movement of the player
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        
+        horizontalInput = Input.GetAxis("Horizontal");  //Get the axis for a boolean value
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime); //code for movement of the player in x axis
+        
+    }
+    // LateUpdate is called after the update function
+    private void LateUpdate()
+    {
+        //if() statement for keeping the player in the negative x axis 
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); //keeping the position same as the range of the screen 
+        }
+        //another if() statement for keeping the player in the positive x axis
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z); //keeping the position same as the range of the screen 
+        }
     }
 }
