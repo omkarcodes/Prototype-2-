2f4fd792e96ad82354ec0edb4e853cc4cc41291f
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-13 08:06:41 +0530

    created spawn positions for the animals to spawn at different locations

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    float spawnRangeX = 20f;
+    float spawnRangeZ = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
